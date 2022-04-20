1ba1bf699d155abadbd2fc215c2ef0e203997e04
Sunny Dhanwani <sunny.dhanwani@joinleadoff.com>
2022-04-20 12:36:37 +0530

    .

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,38 @@
-import HeroSection from "../Components/HeroSection";
+// import HeroSection from "../Components/HeroSection";
 import { useRef, useState, useEffect } from "react";
 
-export default function Home() {
-
-  const slide0 = useRef();
-
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, [])
-
-  return (
-
-    !mounted
-      ? null
-      : <div className={`mandatory-snap`}>
-
-        <div
-          className="snap-parent"
-          ref={slide0}
-        >
-          <HeroSection />
-        </div>
-      </div >
-  )
-}
-
 // export default function Home() {
+
+//   const slide0 = useRef();
+
+//   const [mounted, setMounted] = useState(false);
+
+//   useEffect(() => {
+//     setMounted(true);
+//   }, [])
+
 //   return (
-//     <div>
-//       <h1 className="text-38 font-bold underline text-center">
-//         Hello world!
-//       </h1>
-//     </div>
+
+//     !mounted
+//       ? null
+//       : <div className={`mandatory-snap`}>
+
+//         <div
+//           className="snap-parent"
+//           ref={slide0}
+//         >
+//           <HeroSection />
+//         </div>
+//       </div >
 //   )
 // }
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-38 font-bold underline text-center">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
