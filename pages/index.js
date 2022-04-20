2d12f8be4c98e9fa598fd9f66a30631f5518c431
Sunny Dhanwani <sunny.dhanwani@joinleadoff.com>
2022-04-20 12:40:07 +0530

    Added footer

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,24 @@
 // import HeroSection from "../Components/HeroSection";
 import { useRef, useState, useEffect } from "react";
-
-// export default function Home() {
-
-//   const slide0 = useRef();
-
-//   const [mounted, setMounted] = useState(false);
-
-//   useEffect(() => {
-//     setMounted(true);
-//   }, [])
-
-//   return (
-
-//     !mounted
-//       ? null
-//       : <div className={`mandatory-snap`}>
-
-//         <div
-//           className="snap-parent"
-//           ref={slide0}
-//         >
-//           <HeroSection />
-//         </div>
-//       </div >
-//   )
-// }
+import Footer from "../Components/Footer";
 
 export default function Home() {
+
   return (
-    <div>
-      <h1 className="termina text-38 font-bold underline text-center">
-        Hello world!
-      </h1>
-    </div>
+    <div className={`mandatory-snap`}>
+      <div className="snap-parent">
+        <Footer />
+      </div>
+    </div >
   )
 }
+
+// export default function Home() {
+//   return (
+//     <div>
+//       <h1 className="termina text-38 font-bold underline text-center">
+//         Hello world!
+//       </h1>
+//     </div>
+//   )
+// }
